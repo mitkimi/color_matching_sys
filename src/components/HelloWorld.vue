a4c7e774770dc4ca3134491ea19bb7f87c137b97
田昊天 <mitkimi@192.168.0.126>
2018-03-12 11:34:39 +0800

    all color

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,13 +24,243 @@ export default {
       colors: [
         {
           id: 1,
-          name: '名字',
-          colors: ['#a1c4fd', '#c2e9fb'],
+          name: 'Un-named Color',
+          colors: ['#30cfd0', '#330867'],
         },
         {
           id: 2,
-          name: '名字2',
+          name: 'Un-named Color',
+          colors: ['#a8edea', '#fed6e3'],
+        },
+        {
+          id: 3,
+          name: 'Un-named Color',
+          colors: ['#5ee7df', '#b490ca'],
+        },
+        {
+          id: 4,
+          name: 'Wild Apple',
+          colors: ['#d299c2', '#fef9d7'],
+        },
+        {
+          id: 5,
+          name: 'Saint Petersburg',
+          colors: ['#f5f7fa', '#c3cfe2'],
+        },
+        {
+          id: 6,
+          name: 'Arielles Smlie',
+          colors: ['#000000', '#000000'],
+        },
+        {
+          id: 7,
+          name: 'Un-named Color',
+          colors: ['#a1c4fd', '#c2e9fb'],
+        },
+        {
+          id: 8,
+          name: 'Un-named Color',
           colors: ['#a4fc79', '#96e6a1'],
+        },
+        {
+          id: 9,
+          name: 'Un-named Color',
+          colors: ['#84fab0', '#8fd3f4'],
+        },
+        {
+          id: 10,
+          name: 'Heavy Rain',
+          colors: ['#cfd9df', '#e2ebf0'],
+        },
+        {
+          id: 11,
+          name: 'Amy Crisp',
+          colors: ['#a6c0fe', '#f68084'],
+        },
+        {
+          id: 12,
+          name: 'Mean Fruit',
+          colors: ['#fccb90', '#d57eeb'],
+        },
+        {
+          id: 13,
+          name: 'Un-named Color',
+          colors: ['#c471f5', '#fa71cd'],
+        },
+        {
+          id: 14,
+          name: 'Un-named Color',
+          colors: ['#a4fc79', '#96e6a1'],
+        },
+        {
+          id: 15,
+          name: 'Un-named Color',
+          colors: ['#48c6ef', '#6f86d6'],
+        },
+        {
+          id: 16,
+          name: 'Fresh Milk',
+          colors: ['#feada6', '#f5efef'],
+        },
+        {
+          id: 17,
+          name: 'Snow Again',
+          colors: ['#e6e9f0', '#eef1f5'],
+        },
+        {
+          id: 18,
+          name: 'February Ink',
+          colors: ['#accbee', '#e7f0fd'],
+        },
+        {
+          id: 19,
+          name: 'Un-named Color',
+          colors: ['#e0c3fc', '#8ec5fc'],
+        },
+        {
+          id: 20,
+          name: 'Un-named Color',
+          colors: ['#f093fb', '#f5576c'],
+        },
+        {
+          id: 21,
+          name: 'Un-named Color',
+          colors: ['#fdfbfb', '#ebedee'],
+        },
+        {
+          id: 22,
+          name: 'Malibu Beach',
+          colors: ['#4facfe', '#00f2fe'],
+        },
+        {
+          id: 23,
+          name: 'New Life',
+          colors: ['#43e97b', '#38f9d7'],
+        },
+        {
+          id: 24,
+          name: 'True Sunset',
+          colors: ['#fa709a', '#fee140'],
+        },
+        {
+          id: 25,
+          name: 'Un-named Color',
+          colors: ['#96fbc4', '#f9f586'],
+        },
+        {
+          id: 26,
+          name: 'Un-named Color',
+          colors: ['#2af598', '#009efd'],
+        },
+        {
+          id: 27,
+          name: 'Un-named Color',
+          colors: ['#cd9cf2', '#f6f3ff'],
+        },
+        {
+          id: 28,
+          name: 'Old Hat',
+          colors: ['#000000', '#000000'],
+        },
+        {
+          id: 29,
+          name: 'Star Wine',
+          colors: ['#000000', '#000000'],
+        },
+        {
+          id: 30,
+          name: 'Deep Blue',
+          colors: ['#6a11cb', '#2575fc'],
+        },
+        {
+          id: 31,
+          name: 'Un-named Color',
+          colors: ['#ff9a9e', '#fad0c4'],
+        },
+        {
+          id: 32,
+          name: 'a18cd1',
+          colors: ['#a18cd1', '#fbc2eb'],
+        },
+        {
+          id: 33,
+          name: 'Un-named Color',
+          colors: ['#fad0c4', '#ffd1ff'],
+        },
+        {
+          id: 34,
+          name: 'Juicy Peach',
+          colors: ['#ffecd2', '#fcb69f'],
+        },
+        {
+          id: 35,
+          name: 'Young Passion',
+          colors: ['#000000', '#000000'],
+        },
+        {
+          id: 36,
+          name: 'Lady Lips',
+          colors: ['#ff9a9e', '#fecfef'],
+        },
+        {
+          id: 37,
+          name: 'Un-named Color',
+          colors: ['#000000', '#000000'],
+        },
+        {
+          id: 38,
+          name: 'Un-named Color',
+          colors: ['#37ecba', '#72afd3'],
+        },
+        {
+          id: 39,
+          name: 'Un-named Color',
+          colors: ['#ebbba7', '#cfc7f8'],
+        },
+        {
+          id: 40,
+          name: 'New York',
+          colors: ['#fff1eb', '#ace0f9'],
+        },
+        {
+          id: 41,
+          name: 'Shy Rainbow',
+          colors: ['#000000', '#000000'],
+        },
+        {
+          id: 42,
+          name: 'Loon Crest',
+          colors: ['#a4fc79', '#96e6a1'],
+        },
+        {
+          id: 43,
+          name: 'Plum Plate',
+          colors: ['#667eea', '#764ba2'],
+        },
+        {
+          id: 44,
+          name: 'Everlasting Sky',
+          colors: ['#fdfcfb', '#e2d1c3'],
+        },
+        {
+          id: 45,
+          name: 'Happy Fisher',
+          colors: ['#89f7fe', '#66a6ff'],
+        },
+        {
+          id: 46,
+          name: 'Blessing',
+          colors: ['#fddb92', '#d1fdff'],
+        },
+        {
+          id: 47,
+          name: 'Sharpeye Eagle',
+          colors: ['#9890e3', '#b14f4cf'],
+        },
+        {
+          id: 48,
+          name: 'Ladoga Bottom',
+          colors: ['#ebc0fd', '#d9ded8'],
         },
       ],
     };
